089b491c4f34221e19dfabb92d5265796c6c29d6
fuomag9 <fuomag9@hotmail.it>
2016-05-11 20:09:34 +0000

    Revert "rewrited code"
    This reverts commit a6b4ec9dff2fdc4240f85de25200e3d6a18cc0d2.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
+
 import telebot
 from telebot import types
 from cryptography.fernet import Fernet
@@ -17,7 +18,9 @@ user_dict = {}
 
 class User:
     def __init__(self, name):
-        self.encrypted_message = None
+        self.name = name
+        self.age = None
+        self.sex = None
         self.key=None
 
 @bot.message_handler(commands=["encrypt"])
@@ -33,25 +36,45 @@ def encode(message):
     risposta(message,key)
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['decrypt'])
-def ottieni_messaggio(message):
-    chat_id = message.chat.id
-    msg = bot.reply_to(message, "Invia il messaggio da decifrare")
-    if message.text.replace("/decrypt")!="":
-        encrypted_message=message.text.replace("/decrypt")
-    else:
-        bot.register_next_step_handler(msg, process_name_step)
-def ottieni_key(message):
+@bot.message_handler(commands=['help', 'start','decrypt'])
+def send_welcome(message):
+    msg = bot.reply_to(message, """\
+Hi there, I am Example bot.
+What's your message
+""")
+    bot.register_next_step_handler(msg, process_name_step)
+
+
+def process_name_step(message):
         chat_id = message.chat.id
-        encrypted_message = message.text
+        name = message.text
+        user = User(name)
         user_dict[chat_id] = user
         msg = bot.reply_to(message, 'key')
-        bot.register_next_step_handler(msg, decifra)
-def decifra(message):
+        bot.register_next_step_handler(msg, process_age_step)
+
+
+def process_age_step(message):
         chat_id = message.chat.id
-        key = message.text
+        age = message.text
         user = user_dict[chat_id]
-        plain_text=Fernet(user.key.encode(encoding='UTF-8')).decrypt(user.encrypted_message.encode(encoding='UTF-8'))
+        user.age = age
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Male', 'Female')
+        msg = bot.reply_to(message, 'What is your gender', reply_markup=markup)
+        bot.register_next_step_handler(msg, process_sex_step)
+
+def process_sex_step(message):
+        chat_id = message.chat.id
+        sex = message.text
+        user = user_dict[chat_id]
+        if (sex == u'Male') or (sex == u'Female'):
+            user.sex = sex
+        else:
+            raise Exception()
+        print(user.age.encode(encoding='UTF-8'))
+        plain_text=Fernet(user.age.encode(encoding='UTF-8')).decrypt(user.name.encode(encoding='UTF-8'))
         risposta(message,"Il messaggio decriptato è il seguente:")
         risposta(message,plain_text.decode(encoding='UTF-8'))
+        bot.send_message(chat_id, 'Nice to meet you ' + user.name + '\n Age:' + str(user.age) + '\n Sex:' + user.sex)
 bot.polling()
