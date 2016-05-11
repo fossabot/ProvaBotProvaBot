764eacdb0234242b997f0d52ec49d510fbf57ef3
fuomag9 <fuomag9@hotmail.it>
2016-05-11 21:22:40 +0200

    rip

--- test.py
+++ test.py
@@ -16,14 +16,24 @@ class User:
         self.name = name
         self.age = None
         self.sex = None
+        self.key=None
 
-
+@bot.message_handler(commands=["encrypt"])
+def encode(message):
+    string=message.text.replace("/encrypt","")
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
+    cipher_text = cipher_suite.encrypt(string.encode(encoding='UTF-8'))
+    risposta(message,"Questo è il tuo messaggio criptato: ")
+    risposta(message,cipher_text.decode(encoding='UTF-8'))
+    risposta(message,"Questa è la tua chiave crittografica")
+    risposta(message,key)
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['help', 'decrypt'])
 def send_welcome(message):
     msg = bot.reply_to(message, """\
 Hi there, I am Example bot.
-What's your name?
+What's your message
 """)
     bot.register_next_step_handler(msg, process_name_step)
 
@@ -34,7 +44,7 @@ def process_name_step(message):
         name = message.text
         user = User(name)
         user_dict[chat_id] = user
-        msg = bot.reply_to(message, 'How old are you?')
+        msg = bot.reply_to(message, 'what is ur key')
         bot.register_next_step_handler(msg, process_age_step)
     except Exception as e:
         bot.reply_to(message, 'oooops')
@@ -42,34 +52,12 @@ def process_name_step(message):
 
 def process_age_step(message):
     try:
-        chat_id = message.chat.id
-        age = message.text
-        if not age.isdigit():
-            msg = bot.reply_to(message, 'Age should be a number. How old are you?')
-            bot.register_next_step_handler(msg, process_age_step)
-            return
-        user = user_dict[chat_id]
-        user.age = age
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup.add('Male', 'Female')
-        msg = bot.reply_to(message, 'What is your gender', reply_markup=markup)
-        bot.register_next_step_handler(msg, process_sex_step)
+     plain_text=Fernet(message.text).decrypt(name.encode(encoding='UTF-8'))
+     risposta(message,"Il messaggio decriptato è il seguente:")
+     risposta(message,plain_text.decode(encoding='UTF-8'))
     except Exception as e:
         bot.reply_to(message, 'oooops')
 
-
-def process_sex_step(message):
-    try:
-        chat_id = message.chat.id
-        sex = message.text
-        user = user_dict[chat_id]
-        if (sex == u'Male') or (sex == u'Female'):
-            user.sex = sex
-        else:
-            raise Exception()
-        bot.send_message(chat_id, 'Nice to meet you ' + user.name + '\n Age:' + str(user.age) + '\n Sex:' + user.sex)
-    except Exception as e:
-        bot.reply_to(message, 'oooops')
 
 
 bot.polling()
