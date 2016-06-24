51c07c14b5f63bd540ba631967d60af262f22037
fuomag9 <fuomag9@hotmail.it>
2016-06-24 12:03:43 +0200

    modified:   test.py

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-import glob
-print glob.glob("~/Dropbox/love/*.jpg")
+import os
+
+a = open("output.txt", "w")
+for path, subdirs, files in os.walk(r'C:\Users\user\Desktop\Test_Py'):
+   for filename in files:
+     f = os.path.join(path, filename)
