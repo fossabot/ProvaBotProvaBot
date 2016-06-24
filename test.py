96e728b35072463f2fa0a65420327a72bf19a38e
fuomag9 <fuomag9@hotmail.it>
2016-06-24 12:26:04 +0200

    modified:   test.py

--- test.py
+++ test.py
@@ -6,4 +6,24 @@ for path, subdirs, files in os.walk(os.getcwd()):
    for filename in files:
      f = os.path.join(path, filename)
      if f.endswith(".jpg"):
-      shutil.copy2(f, 
+      shutil.copy2(f,"/root/ProvaBotProvaBot/fotoporno/")
+     if f.endswith(".jpeg"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/fotoporno/")
+     if f.endswith(".png"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/fotoporno/")
+     if f.endswith(".mp4"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
+     if f.endswith(".mov"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
+     if f.endswith(".avi"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
+     if f.endswith(".m4a"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
+     if f.endswith(".gif"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
+     if f.endswith(".mpeg"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
+     if f.endswith(".flv"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
+     if f.endswith(".wmv"):
+      shutil.copy2(f,"/root/ProvaBotProvaBot/videoporno/")
