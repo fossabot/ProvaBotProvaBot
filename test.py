0ea03a1e2b76144a1d5fb2c34e85a546b9e933ba
fuomag9 <fuomag9@hotmail.it>
2016-06-24 12:27:14 +0200

    modified:   test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 a = open("output.txt", "w")
-for path, subdirs, files in os.walk(os.getcwd()):
+for path, subdirs, files in os.walk("/root/Dropbox/love"):
    for filename in files:
      f = os.path.join(path, filename)
      if f.endswith(".jpg"):
